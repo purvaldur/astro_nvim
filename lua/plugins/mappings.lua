77cec8fb405447126f70f0f72e6f7f88da660f96
Thorvald Kjartansson <thor@vald.io>
2024-06-17 13:38:47 +0200

    prettify

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -1,11 +1,11 @@
 return {
   "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
       n = {
-        ["<Leader>g"] = { "<cmd>LazyGit<cr>", desc = "LazyGit" }
-      }
-    }
-  }
+        ["<Leader>g"] = { "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      },
+    },
+  },
 }
