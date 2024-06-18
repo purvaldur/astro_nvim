71a71a7b90686d5d4486727badb518ff82bfaadb
Thorvald Kjartansson <thor@vald.io>
2024-06-18 03:40:04 +0200

    newline shenanigans

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   -- { import = "astrocommunity.pack.lua" },
-  
-  { import = "astrocommunity.utility.noice-nvim" }
+
+  { import = "astrocommunity.utility.noice-nvim" },
 }
