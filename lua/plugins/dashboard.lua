9f7cf81ab8ed070f8825ecd448ab92f5ab43b969
Thorvald Kjartansson <thor@vald.io>
2024-06-18 03:40:53 +0200

    remove unused var

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,6 +1,6 @@
 return {
   "goolord/alpha-nvim",
-  opts = function(_, opts) -- override the options using lazy.nvim
+  opts = function() -- override the options using lazy.nvim
     local dashboard = require "alpha.themes.dashboard"
 
     dashboard.section.header.val = { -- change the header section value
